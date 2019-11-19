0c7c075b75c410d415b7a33bbe1c281e81336c04
Alex <alex@fischman.org>
2019-11-18 21:22:25 -0800

    Update Main.hs

--- Main.hs
+++ Main.hs
@@ -1,3 +1,13 @@
 import Neurode
 
-main = print $ gradient (\[x,y] -> log x + 3 * y) [0.1, 3]
+main :: IO ()
+main = print $ map (fromRational :: Rational -> Double) $ (!!10) $ iterate (train model training) state
+
+training :: (Num a, Enum a) => [([a], a)]
+training = [([a,b], a + b) | a <- [-10..10], b <- [-10..10]]
+
+state :: [Rational]
+state = [3 / 2, 2]
+
+model :: Num a => [a] -> [a] -> a
+model = (.) sum . zipWith (*)
