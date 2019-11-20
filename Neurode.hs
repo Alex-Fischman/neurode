eb115f2843e8930786a5338619371f2a23a8cb7f
Alex <alex@fischman.org>
2019-11-20 00:03:09 -0800

    Update Neurode.hs

--- Neurode.hs
+++ Neurode.hs
@@ -2,11 +2,14 @@ module Neurode (train) where
 
 import Dual
 
-train model training start = descend (\state -> sum . map (\(input,output) -> loss (model state input) output) $ training) start
+train :: (Eq a, Fractional a) => ([Dual a] -> b -> [Dual a]) -> [(b, [Dual a])] -> [a] -> [a]
+train model training = descend (\state -> sum . map (\(input,output) -> loss (model state input) output) $ training)
     where loss = (.) sum . zipWith (\a b -> (a - b) * (a - b))
-    
+
+descend :: (Eq a, Fractional a) => ([Dual a] -> Dual a) -> [a] -> [a]
 descend f xs = zipWith (-) xs . map (\x -> if x == 0 then 0 else result / x) . gradient f $ xs
     where result = evaluate . f . zipWith Dual xs $ repeat 0
 
+gradient :: Num a => ([Dual a] -> Dual b) -> [a] -> [b]
 gradient f xs = map (differentiate . f . zipWith Dual xs) . i . length $ xs
     where i n = iterate ((:) (1:repeat 0) . map (0:)) [] !! n
